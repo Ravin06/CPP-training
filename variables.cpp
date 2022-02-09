c67130d4055605887b71dcb4fcd4d8425f2e9230
Ravin06 <77960307+Ravin06@users.noreply.github.com>
2022-02-09 11:11:06 +0800

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNum = 15;   // Now myNum is 15
+  myNum = 10;       // Now myNum is 10
+  cout << myNum;
+  int myNum = 5;    // Integer (whole number without decimals)
+  double myFloatNum = 5.99; // Floating point number (with decimals)
+  char myLetter = 'D'; // Character
+  string myText = "Hello"; // String (text)
+  bool myBoolean = true; // Boolean (true or false)
+  return 0;
+}
