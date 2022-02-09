6fd8a0e3a2c243447198fd87b8ffbb088e0dcc51
Ravin06 <77960307+Ravin06@users.noreply.github.com>
2022-02-09 10:58:59 +0800

    Create PrintfilesNewLineComments.cpp

---
+++ PrintfilesNewLineComments.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  /*multiline comments 
+  another line
+  another line
+  */
+  //code comments haha
+  std::cout << "Test? \n";
+  //printing test ^
+  std::cout << "C++ kool \n\n";
+  //printing C++ kool ^
+  std::cout << "hehe print \n";
+  //printing hehe print ^
+  std::cout << "This is the 4th line \n";
+  //printing 4th line ^
+  cout << "using endl" << endl;
+  //using endl ^
+  return 0;
+}
